cf007fe7e88f3b341fb6f177f6746ae375cb9a3f
Greg Albers <galbers@getty.edu>
2015-09-12 15:47:23 -0700

    JS improvements scroll list, closes #7
    smooth scroll, external js. Had to add jquery for animate.

--- assets/js/ce.js
+++ assets/js/ce.js
@@ -4,4 +4,21 @@ function setLayout() {
 	var i;
 	for (i = 0; i < x.length; i++) {
 	    x[i].style.width = w + "px";
-}}
+		}
+	};
+
+document.getElementById('scroll').style.overflowX = "hidden";	
+
+function scrollNext() {
+	var wdth = document.getElementById('scroll').offsetWidth;
+	var scrl = wdth * .8;
+	var scrollPos = document.getElementById('scroll').scrollLeft;
+	$('#scroll').animate({ scrollLeft: scrollPos + scrl }, 500);
+	};
+
+function scrollPrev() {
+	var wdth = document.getElementById('scroll').offsetWidth;
+	var scrl = wdth * .8;
+	var scrollPos = document.getElementById('scroll').scrollLeft;
+	$('#scroll').animate({ scrollLeft: scrollPos - scrl }, 500);
+	}
